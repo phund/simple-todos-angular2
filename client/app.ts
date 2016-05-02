50006fa69f1f6ede92e0560e6b6a242519b8189a
phund <itghost90@gmail.com>
2016-05-02 23:27:44 +0700

    Add tasks

--- client/app.ts
+++ client/app.ts
@@ -1,12 +1,22 @@
-import 'reflect-metadata';
 import 'zone.js/dist/zone';
-import {Component} from 'angular2/core';
-import {bootstrap} from 'angular2/platform/browser';
- 
+import 'reflect-metadata';
+
+import {Component, NgZone, provide, enableProdMode} from 'angular2/core';
+
+// import {Tasks} from '../imports/api/tasks';
+
+import {TaskList} from '../imports/components/tasks-list/tasks-list';
+
+import {bootstrap} from 'angular2-meteor-auto-bootstrap';
+
+enableProdMode();
+
 @Component({
   selector: 'app',
-  templateUrl: 'client/app.html'
+  templateUrl: 'client/app.html',
+  directives: [TaskList]
 })
-class Todos { }
- 
+export class Todos {
+}
+
 bootstrap(Todos);
---
+++ imports/components/tasks-list/tasks-list.ts
@@ -0,0 +1,52 @@
+
+import {Component, Input, OnChanges} from 'angular2/core';
+
+import {TaskView} from '../task/task';
+
+import {MeteorComponent} from 'angular2-meteor';
+
+import { Tasks } from '../../api/tasks';
+
+import {Mongo} from "meteor/mongo";
+
+@Component({
+  selector: 'task-list',
+  templateUrl: 'imports/components/tasks-list/tasks-list.html',
+  directives: [TaskView]
+})
+export class TaskList extends MeteorComponent implements OnChanges {
+  tasks: Mongo.Cursor<Task>;
+  @Input() hideCompleted: boolean = false;
+  isLoading: boolean;
+
+  constructor() {
+    super();
+    this.isLoading = true;
+    this.subscribe('tasks.public', () => {
+      this.isLoading = false;
+    }, true);
+  }
+
+  ngOnChanges() {
+    this.tasks = this._getTasks(this.hideCompleted);
+  }
+
+  addTask(text) {
+    this.call('tasks.addTask', text);
+  }
+
+  get todoCount() {
+    return Tasks.find({
+      checked: false
+    }).count();
+  };
+
+  _getTasks(hideCompleted) {
+    if (hideCompleted) {
+      return Tasks.find({
+        checked: false
+      });
+    }
+    return Tasks.find({});
+  }
+}
